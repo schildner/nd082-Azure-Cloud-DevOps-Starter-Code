1b0485e8c4e14c58467089630a82ab6afc61783e
schildner <eduard.schildner@gmail.com>
2020-12-14 21:31:13 +0100

    Add basic load balancer.

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -33,6 +33,23 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool"
+}
+
 resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-nsg"
   location            = azurerm_resource_group.main.location
